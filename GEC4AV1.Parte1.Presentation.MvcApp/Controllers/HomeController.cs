df4c765079731b09b14ce7b2920aaeebc981e275
Matheus Bitencourt <Matheus Bitencourt>
2016-02-28 18:20:19 -0300

    last commit

---
+++ GEC4AV1.Parte1.Presentation.MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GEC4AV1.Parte1.Presentation.MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+        // GET: Services
+        public ActionResult Services()
+        {
+            return View();
+        }
+        // GET: Works
+        public ActionResult Works()
+        {
+            return View();
+        }
+    }
+}
---
+++ GEC4AV1.Parte1.Presentation.MvcApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GEC4AV1.Parte1.Presentation.MvcApp.MvcApplication" Language="C#" %>
